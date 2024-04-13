1dd80995bfabafff927b29982488d9eab7871c25
Alex Zappa <alex@zappa.dev>
2024-04-13 03:37:23 +0200

    Update Plausible API domain in tracker.js
    Domain and apiHost properties have been updated to point to new URLs in tracker.js. An additional property "project" is introduced, which is marked as "cloudflare.serverless-gems.dev". These changes will allow the tracking to align with the new server and project setup.

--- src/assets/js/modules/tracker.js
+++ src/assets/js/modules/tracker.js
@@ -3,8 +3,8 @@ import Plausible from "plausible-tracker";
 import colorScheme from "./_detectColorScheme";
 
 const {trackEvent, trackPageview, enableAutoOutboundTracking} = Plausible({
-  domain: "cloudflare.serverless-gems.dev",
-  apiHost: "https://plausible.serverless-gems.dev",
+  domain: "serverless-gems.dev",
+  apiHost: "https://firebird.sunnypixels.workers.dev/api/event",
 });
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -23,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
         prefersColorScheme: colorScheme(),
         userAgent: navigator.userAgent,
         deviceWidth: window.innerWidth,
+        project: "cloudflare.serverless-gems.dev"
       },
     }
   );
